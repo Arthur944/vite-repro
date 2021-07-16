4e5dc242baf9d2fcb74f3d827f49d688729a20d8
Arthur944 <23a23r23t23@gmail.com>
2021-07-16 12:48:06 +0200

    Changed to yarn berry pnp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {FieldWrapper, InputField} from '@litbase/alexandria';
+import { TempTestFormikk } from '@litbase/alexandria';
 import { Formik } from 'formik';
 
 function App() {
   return (
     <div className="App">
-        <Formik initialValues={{test: ""}} onSubmit={() => {}}>
-            <FieldWrapper name={'test'} component={InputField} />
-        </Formik>
+        <h1 style={{fontSize: 128 }}>
+            {String(TempTestFormikk === Formik)}
+        </h1>
     </div>
   )
 }
